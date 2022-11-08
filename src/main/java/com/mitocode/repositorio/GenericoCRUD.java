8aaae2b75603f8ec82e98766c5aa39ca70300d14
Carlos Andres Pacheco Fuentes <carloandres4020@gmail.com>
2022-11-07 21:58:44 -0500

    Se refactoriza el codigo para mejorar su lectura

--- src/main/java/com/mitocode/repositorio/GenericoCRUD.java
+++
@@ -1,9 +0,0 @@
-package com.mitocode.repositorio;
-
-import com.mitocode.modelo.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenericoCRUD extends JpaRepository<Paciente, Integer> {
-
-
-}
